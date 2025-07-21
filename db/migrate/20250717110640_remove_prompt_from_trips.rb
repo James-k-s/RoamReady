83c0a9d746aaddf2f87d814c35fe054dc6a9b7b2
James Kay-Shuttleworth <kayshuttleworthjames@gmail.com>
2025-07-21 16:31:44 +0100

    this version includes pstgresql

--- db/migrate/20250717110640_remove_prompt_from_trips.rb
+++
@@ -1,5 +0,0 @@
-class RemovePromptFromTrips < ActiveRecord::Migration[7.1]
-  def change
-    remove_column :trips, :propmt, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,15 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2025_07_21_135350) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "accommodations", force: :cascade do |t|
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.string "name"
     t.string "booking_url"
     t.string "address"
-    t.integer "trip_id", null: false
+    t.bigint "trip_id", null: false
     t.boolean "booked"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -31,7 +34,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_07_21_135350) do
     t.integer "companions"
     t.string "location"
     t.string "activity"
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "extra_information"
